d7b5549d9f7edadf8d2c6fffeafec83429ce058c
Al Ambari <al.ambari@usetada.com>
2022-02-10 08:16:47 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(process.env.port || 5003);
+
+console.log('Running at Port 5003');
